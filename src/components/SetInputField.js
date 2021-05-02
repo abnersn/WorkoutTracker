9676193c9989b186c92e994740d4c1a00ac4c3cd
Abner Nascimento <abnersousanascimento@gmail.com>
2021-05-02 00:50:22 -0300

    Introduce special input field component for sets

---
+++ src/components/SetInputField.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function SetInputField(props) {
+    const {
+        inputProps,
+        labelText,
+        defaultValue = null,
+        formatFunction = val => val
+    } = props;
+    const [value, setValue] = useState(defaultValue);
+    const [isEdit, setIsEdit] = useState(false);
+
+    return (
+        <label onFocus={() => setIsEdit(true)} onBlur={() => setIsEdit(false)}>
+            <span>{labelText}</span>
+            {
+                isEdit ? (
+                    <input
+                        {...inputProps}
+                        value={value}
+                        onChange={ev => setValue(ev.target.value)}
+                    ></input>
+                ) : (
+                    <span>
+                        {formatFunction(value)}
+                    </span>
+                )
+            }
+        </label>
+    )
+}
---
+++ src/tests/SetInputField.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act, Simulate } from 'react-dom/test-utils';
+
+import SetInputField from '../components/SetInputField';
+import timeFormat from '../util/timeFormat';
+
+let container = null;
+
+beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+const component = <SetInputField
+    defaultValue={30}
+    formatFunction={timeFormat}
+></SetInputField>;
+
+describe('SetInputField component', () => {
+    it('starts as display only', () => {
+        act(() => {
+            render(component, container);
+        });
+        const $input = container.querySelector('input');
+    
+        expect($input).toBeFalsy();
+    });
+
+    it('enables edit when focused', () => {
+        act(() => {
+            render(component, container);
+        });
+        const $label = container.querySelector('label');
+        Simulate.focus($label);
+        const $input = container.querySelector('input');
+    
+        expect($input).toBeTruthy();
+    });
+
+    it('disables edit when focused', () => {
+        act(() => {
+            render(component, container);
+        });
+        const $label = container.querySelector('label');
+        Simulate.blur($label);
+        const $input = container.querySelector('input');
+    
+        expect($input).toBeFalsy();
+    });
+});
