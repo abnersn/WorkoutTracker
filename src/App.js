66101999afcbb2067c185faa48e4eadbec0c8c02
Abner Nascimento <abnersousanascimento@gmail.com>
2021-05-05 19:53:54 -0300

    New loading screen

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import Workout from './components/Workout';
 
 import './i18n';
 
+function Loading() {
+    return(
+        <div className='bg-white p-2 m-3 rounded-lg shadow max-w-md mx-auto'>
+            Loading...
+        </div>
+    );
+}
+
 function App() {
     return (
-        <Suspense fallback={<p>Loading...</p>}><Workout /></Suspense>
+        <Suspense fallback={<Loading />}><Workout /></Suspense>
     );
 }
 
