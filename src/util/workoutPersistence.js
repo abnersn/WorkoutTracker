d636c16d97cd808ee92711f41535717a56f81ab8
Abner Nascimento <abnersousanascimento@gmail.com>
2021-05-07 17:06:07 -0300

    Create indexeddb manager

--- src/util/workoutPersistence.js
+++ src/util/workoutPersistence.js
@@ -1,5 +1,93 @@
 import serializeWorkout from '../util/serializeWorkout';
 
+export default class Database {
+    constructor() {
+        this.db = null;
+    }
+    async init() {
+        if (!('indexedDb' in window)) {
+            alert('This app needs indexedDb support to run.');
+            return;
+        }
+        this.db = await indexedDB.open('workoutTracker', 1, (upgradeDb) => {
+            if (!upgradeDb.objectStoreNames.contains('workouts')) {
+                upgradeDb.createObjectStore('workouts', { keyPath: 'id' });
+            }
+            if (!upgradeDb.objectStoreNames.contains('logs')) {
+                upgradeDb.createObjectStore('logs', { autoIncrement: true });
+            }
+        });
+    }
+
+    getAllData(storeName) {
+        const transaction = this.db.transaction(storeName, 'readonly');
+        const store = transaction.objectStore(storeName);
+        return store.getAll();  
+    }
+
+    getData(storeName, key) {
+        const transaction = this.db.transaction(storeName, 'readonly');
+        const store = transaction.objectStore(storeName);
+        return store.get(key);  
+    }
+
+    runInWorkouts(operation) {
+        const transaction = this.db.transaction('workouts', 'readwrite');
+        const store = transaction.objectStore('workouts');
+        operation(store);
+        return transaction.complete;  
+    }
+
+    runInLogs(operation) {
+        const transaction = this.db.transaction('logs', 'readwrite');
+        const store = transaction.objectStore('logs');
+        operation(store);
+        return transaction.complete;  
+    }
+
+    saveLogEntry(logEntry) {
+        return this.runInLogs((store) => store.add(logEntry));
+    }
+
+    eraseLogEntry(key) {
+        return this.runInLogs((store) => store.delete(key));
+    }
+
+    eraseWorkout(id) {
+        return this.runInWorkouts((store) => store.delete(id));
+    }
+
+    async getLastLogEntry(workoutId) {
+        const lastLogEntry = (await this.getAllData('logs'))
+            .filter(entry => entry.id === workoutId)
+            .sort((a, b) => b.date - a.date);
+
+            return lastLogEntry;
+    }
+
+    async loadWorkoutLogById(id, createNew = true) {
+        const lastLogEntry = await this.getLastLogEntry(id);
+    
+        const workout = lastLogEntry || await this.getData('workouts', id);
+    
+        if (createNew) {
+            workout.date = new Date();
+            workout.isComplete = false;
+            workout.duration = 0;
+        } else {
+            workout.date = new Date(workout.date);
+        }
+    
+        for (const exercise of workout.exercises) {
+            for (const set of exercise.sets) {
+                set.stage = createNew ? 'IDLE' : 'COMPLETE';
+            }
+        }
+    
+        return workout;
+    }
+}
+
 export function workoutHistoryList() {
     const { localStorage } = window;
 
